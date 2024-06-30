6b7ceaf84df593fafd02bbb1c9af9f4635e3b3e4
s.kurimoto <105426647+Krimot@users.noreply.github.com>
2024-07-01 00:57:08 +0900

    元のテキストに空の行があるとmdの表に空の列ができていたので削除

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // githubに複数枚貼り付けた画像のURLをMarkdownの表に変換する
 document.getElementById('inputText').addEventListener('input', function() {
     var inputText = document.getElementById('inputText').value;
-    var elements = inputText.split('\n'); // 入力テキストを行ごとに分割
+    var elements = inputText.split('\n').filter(function(element) {
+        return element.trim() !== ''; // 空の行を削除
+    });
     var header =  new Array(elements.length + 1).join('| ') + '|' ; // ヘッダー行（ | | | | ）を生成
     var outputLine = '|' + elements.join('|') + '|'; // 要素をMarkdownの表形式に変換( |111|222|333|444| )
     var outputText = header + '\n|' + new Array(elements.length + 1).join('---|') + '\n' + outputLine; // ヘッダー行と|---|---|---|と変換した要素を結合
